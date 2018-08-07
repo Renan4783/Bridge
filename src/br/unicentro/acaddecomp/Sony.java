f697157c292299daba6c03ced6bc7df137838ee7
ranemj77 <renanmj77@gmail.com>
2018-08-07 19:26:19 -0300

    versao original

---
+++ src/br/unicentro/acaddecomp/Sony.java
@@ -0,0 +1,26 @@
+package br.unicentro.acaddecomp;
+
+public class Sony implements TV{
+
+	private boolean onoff = false;
+	private int channel;
+	
+	@Override
+	public void on() {
+		onoff = true;
+		System.out.println("A TV está ligada");
+	}
+
+	@Override
+	public void off() {
+		onoff = false;
+		System.out.println("A TV está desligada");
+		
+	}
+
+	@Override
+	public void tuneChannel(int channel) {
+		this.channel = channel;
+	}
+	
+}
